2c91bef8079c47c8f4aa56d0bbfdd8f6e244f896
Heyi Sun <heyisun@buffalo.edu>
2019-10-28 11:51:34 -0400

    sendMeEmail.sh acquire mx server based on domain

--- sendMeEmail.sh
+++ sendMeEmail.sh
@@ -1,14 +1,13 @@
 #/usr/bin/env bash
 my_email_address=$1
 domain_address=$(echo $my_email_address | cut -d "@" -f2)
-echo $domain_address
+echo "Parse the domain address: $domain_address"
 canonical_address=$(nslookup -q=mx $domain_address | grep $domain_address | head -n 1 |awk '{print $NF}')
-echo $canonical_address
+echo "Lookup the canonical address of the domain:" $canonical_address
 
 if [[ $? == 0 ]];then
     cp send.txt send_temperory.txt
     sed -i "s/targetEmail/$my_email_address/" send_temperory.txt
-    telnet mxa-00256a01.gslb.pphosted.com 25 < send_temperory.txt
-    #telnet $canonical_address 25 < send_temperory.txt
+    sleep 1 | telnet $canonical_address 25 < send_temperory.txt
     rm send_temperory.txt
 fi

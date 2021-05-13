c2c16a707d5f96eb73515861e2551518151a9eef
Heyi Sun <hs3880@nyu.edu>
2021-05-13 17:41:00 +0000

    Add vim colorscheme and YCM config

--- vim_config/init.sh
+++ vim_config/init.sh
@@ -1,5 +1,37 @@
-sudo apt-get intsall build-essential cmake
-sudo apt-get intsall cppcheck
-sudo apt-get intsall universal-ctags
-sudo apt-get install global
-cmake python-dev python3-dev
+#!/bin/bash
+
+sudo apt-get -y install build-essential cmake
+sudo apt-get -y install cppcheck
+sudo apt-get -y install universal-ctags
+sudo apt-get -y install global cmake python-dev python3-dev
+
+# Install vim-plug
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# Add swapfile to compile YCM
+sudo fallocate -l 1G ~/swapfile
+sudo dd if=/dev/zero of=~/swapfile bs=1024 count=1048576
+sudo chmod 600 ~/swapfile
+sudo mkswap ~/swapfile
+sudo swapon ~/swapfile
+
+# verify swapfile
+sudo swapon --show
+sudo free -h
+
+# compile YCM
+cd ~/.vim/plugged/YouCompleteMe/
+./install.py --clang-completer
+cd -
+
+# clean the swapfile
+sudo swapoff ~/swapfile
+sudo rm ~/swapfile
+
+# mv YCM conf file
+mv .ycm_extra_conf.py ~/.vim/
+
+# add color scheme
+mkdir -p ~/.vim/colors/
+mv ./molokai.vim ~/.vim/colors
